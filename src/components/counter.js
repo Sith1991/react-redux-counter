c5fa0fb580e1d4127b1c2ea523bae88d5521638c
Fiodor Furick <fff24.06.91@gmail.com>
2021-01-26 11:31:58 +0200

    refactor (delete) mapDispatchToProps in counter.js bcse actions are object

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {connect} from "react-redux";
 import * as actions from '../actions';
-import {bindActionCreators} from "redux";
 
 const Counter = ({counter, dec, inc, rnd}) => {
     return (
@@ -20,16 +19,4 @@ const mapStateToProps = (state) => {
     }
 }
 
-const mapDispatchToProps = (dispatch) => {
-    const {inc, dec, rnd} = bindActionCreators(actions, dispatch)
-    return {
-        inc: ()=> inc(),
-        dec: ()=> dec(),
-        rnd: ()=> {
-            const payload = Math.floor(Math.random()*10);
-            return rnd(payload);
-        }
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default connect(mapStateToProps, actions)(Counter);
